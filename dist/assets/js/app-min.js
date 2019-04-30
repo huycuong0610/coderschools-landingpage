579ed2fcf56115838c75bf8c0034d52c554bf1d2
huycuong0610 <huycuong0610@gmail.com>
2019-04-30 11:15:52 +0700

    update 30/4

--- dist/assets/js/app-min.js
+++ dist/assets/js/app-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){console.log("ready!")});
+$(document).ready(function(){console.log("ready!"),$(function(){$("#contact-form").on("submit",function(t){t.preventDefault();return $.ajax({type:"POST",url:"data.php",data:$(this).serialize(),success:function(t){var e="alert-"+t.type,a=t.message,s='<div class="alert '+e+' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'+a+"</div>";e&&a&&($("#contact-form").find(".messages").html(s),$("#contact-form")[0].reset())}}),!1})})});
